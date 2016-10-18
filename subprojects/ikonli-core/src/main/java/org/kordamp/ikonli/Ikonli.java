9227369577d819000929ba5d3fe3fea406f70fdd
aalmiray <aalmiray@gmail.com>
2016-10-18 14:01:41 +0200

    Define an empty icon as a placeholder

---
+++ subprojects/ikonli-core/src/main/java/org/kordamp/ikonli/Ikonli.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2015-2016 Andres Almiray. <aalmiray@yahoo.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kordamp.ikonli;
+
+/**
+ * @author Andres Almiray
+ */
+public enum Ikonli implements Ikon {
+    NONE("ikn-none",'\ue600');
+
+    public static Ikonli findByDescription(String description) {
+        for (Ikonli font : values()) {
+            if (font.getDescription().equals(description)) {
+                return font;
+            }
+        }
+        throw new IllegalArgumentException("Icon description '" + description + "' is invalid!");
+    }
+
+    private String description;
+    private char code;
+
+    Ikonli(String description, char code) {
+        this.description = description;
+        this.code = code;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public char getCode() {
+        return code;
+    }
+}
