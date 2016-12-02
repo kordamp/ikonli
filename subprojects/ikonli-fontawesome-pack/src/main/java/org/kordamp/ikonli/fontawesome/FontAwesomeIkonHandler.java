b33d655847d4edcb598e6f2045ecfbad52a4653a
aalmiray <aalmiray@gmail.com>
2016-12-02 16:35:15 +0100

    Update FontAwesome to version 4.7.0

--- subprojects/ikonli-fontawesome-pack/src/main/java/org/kordamp/ikonli/fontawesome/FontAwesomeIkonHandler.java
+++ subprojects/ikonli-fontawesome-pack/src/main/java/org/kordamp/ikonli/fontawesome/FontAwesomeIkonHandler.java
@@ -37,7 +37,7 @@ public class FontAwesomeIkonHandler extends AbstractIkonHandler {
 
     @Override
     public String getFontResourcePath() {
-        return "META-INF/resources/fontawesome/4.6.3/fonts/fontawesome-webfont.ttf";
+        return "META-INF/resources/fontawesome/4.7.0/fonts/fontawesome-webfont.ttf";
     }
 
     @Override
